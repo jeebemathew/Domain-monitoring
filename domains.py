ec650483c475bc9d3bccbd2fc50076b1563ff654
root <root@ip-172-31-32-118.us-east-2.compute.internal>
2020-08-04 19:27:30 +0000

    status_code verified

---
+++ domains.py
@@ -0,0 +1,40 @@
+import requests
+
+urls = [
+  'https://freevideolectures.com',
+  'https://www.guru99.com/',
+  'https://linuxjourney.com',
+  'https://www.quora.com',
+  'https://www.tecmint.com',
+  'https://linuxsurvival.com',
+  'https://www.tutorialspoint.com',
+  'https://www.computerworld.com',
+  'https://hackernoon.com',
+  'https://www.howtoforge.com',
+  'https://asdfg.com',
+  'https://www.ubuntupit.com/',
+  'https://www.techworm.net',
+  'https://www.2daygeek.com/',
+  'https://www.linux.org',
+  'https://linuxacademy.com',
+  'https://www.cyberciti.biz',
+  'https://itsfoss.com',
+  'https://blog.feedspot.com',
+  'https://www.techradar.com',
+  'https://phoenixts.com/',
+  'https://training.linuxfoundation.org',
+  'https://www.reallinuxuser.com/',
+  'https://linuxconfig.org',
+  'https://sourceforge.net',
+  'https://www.makeuseof.com'
+]
+
+for url in urls:
+    response = requests.get(url)
+
+    if response.status_code == requests.codes.OK:
+        print('UP', url,response.status_code)
+    
+    else:
+        print('DOWN', url, response.status_code)
+
